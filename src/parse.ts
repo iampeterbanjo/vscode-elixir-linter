21ddf99c279fa9559efe5b20297a83513d678827
iampeterbanjo <peterbanjo@gmail.com>
2017-02-28 06:11:04 +0000

    refactored parse.getColumn into parse.getLineInfo and passed line position tests

--- src/parse.ts
+++ src/parse.ts
@@ -1,14 +1,25 @@
+interface LineInfo {
+  column?: number,
+  position?: number
+}
+
 export let getLines = (output): string[] => {
   return output.split('\n');
 }
 
-export let getColumn = (line): number => {
-  let column,
-      sections = line.split(':');
+export let getLineInfo = (line): LineInfo => {
+  let info: any = {},
+      sections: string[] = line.split(':'),
+      positionSection = sections[1],
+      columnSection = sections[2];
 
-  if (sections[2]) {
-    column = +/\d+/.exec(sections[2])[0];
+  if (positionSection) {
+    info.position = +/\d+/.exec(positionSection)[0];
   }
 
-  return column;
+  if (columnSection) {
+    info.column = +/\d+/.exec(columnSection)[0];
+  }
+
+  return info;
 }
--- src/parse_spec.ts
+++ src/parse_spec.ts
@@ -25,15 +25,23 @@ describe('parse', () => {
     });
   });
 
-  describe('.getColumn', () => {
+  describe('.getLineInfo', () => {
+    let testTree: any = [
+          {line: 'nothing to see here', expected: undefined},
+          {line: '[R] → lib/rumbl.ex:27:7 Functions should have a @spec type specification.', position: 27, column: 7},
+          {line: '[R] → lib/rumbl.ex:1:11 Modules should have a @moduledoc tag.', position: 1, column: 11},
+          {line: '[R] → lib/rumbl.ex:6:1 Functions should have a @spec type specification.', position: 6, column: 1}
+        ];
+
     it('should get expected line column', () => {
-      [
-        {line: 'nothing to see here', expected: undefined},
-        {line: '[R] → lib/rumbl.ex:27:7 Functions should have a @spec type specification.', expected: 7},
-        {line: '[R] → lib/rumbl.ex:1:11 Modules should have a @moduledoc tag.', expected: 11},
-        {line: '[R] → lib/rumbl.ex:6:1 Functions should have a @spec type specification.', expected: 1}
-      ].forEach(t => {
-        assert.equal(parse.getColumn(t.line), t.expected);
+      testTree.forEach(t => {
+        assert.equal(parse.getLineInfo(t.line).column, t.column);
+      });
+    });
+
+    it('should get expected position', () => {
+      testTree.forEach(t => {
+        assert.equal(parse.getLineInfo(t.line).position, t.position);
       });
     });
   });
