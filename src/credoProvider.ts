90d1f85bdfe787c2b4eb8b981530de3fbd2c80cd
iampeterbanjo <peterbanjo@gmail.com>
2018-05-02 21:31:10 +0100

    try and create file URI - failed

--- src/credoProvider.ts
+++ src/credoProvider.ts
@@ -1,9 +1,9 @@
 "use strict";
 
 import * as cp from "child_process";
-import * as path from "path";
 import * as execa from "execa";
 import * as getStream from "get-stream";
+import * as path from "path";
 
 import * as cmd from "./command";
 import * as parse from "./parse";
@@ -13,171 +13,202 @@ import * as severity from "../src/severity";
 import IdeExtensionProvider from "./ideExtensionProvider";
 
 export default class ElixirLintingProvider {
-    private static linterCommand: string = "mix";
+  private static linterCommand: string = "mix";
 
-    private command: any;
+  private command: any;
 
-    private diagnosticCollection: any;
+  private diagnosticCollection: any;
 
-    private extension: any;
+  private extension: any;
 
-    private vscode: any;
+  private vscode: any;
 
-    constructor(vscode) {
-        this.vscode = vscode;
-        this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
-        this.extension = new IdeExtensionProvider(this.diagnosticCollection, this.command);
+  constructor(vscode) {
+    this.vscode = vscode;
+    this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
+    this.extension = new IdeExtensionProvider(
+      this.diagnosticCollection,
+      this.command,
+    );
+  }
+
+  /**
+   * activate() and dispose() deal with set-up and tear-down in VS Code extensions.
+   * The code below registers the command so that the CodeActionProvider can call it and
+   *  sets up listeners to trigger the linting action.
+   */
+
+  public activate(subscriptions: any[], vscode = this.vscode) {
+    this.extension.activate(this, subscriptions, vscode, this.linter);
+
+    // Lint all open elixir documents
+    vscode.workspace.textDocuments.forEach((item, index) => {
+      this.linter(item, index, vscode);
+    });
+  }
+
+  public dispose(): void {
+    this.extension.dispose();
+  }
+
+  // getDiagnosis for vscode.diagnostics
+  public getDiagnosis(item, vscode = this.vscode): any {
+    const range = new vscode.Range(
+      item.startLine,
+      item.startColumn,
+      item.endLine,
+      item.endColumn,
+    );
+    const itemSeverity = severity.parse(item.check, vscode);
+    const message = `${item.message} [${item.check}:${itemSeverity}]`;
+    return new vscode.Diagnostic(range, message, itemSeverity);
+  }
+
+  public makeZeroIndex = (value: number): number => {
+    if (value <= 0) {
+      return 0;
     }
 
-    /**
-     * activate() and dispose() deal with set-up and tear-down in VS Code extensions.
-     * The code below registers the command so that the CodeActionProvider can call it and
-     *  sets up listeners to trigger the linting action.
-     */
+    return value - 1;
+  }
 
-    public activate(subscriptions: any[], vscode = this.vscode) {
-        this.extension.activate(this, subscriptions, vscode, this.linter);
-
-        // Lint all open elixir documents
-        vscode.workspace.textDocuments.forEach((item, index) => {
-            this.linter(item, index, vscode);
-        });
+  public getDiagnosticInfo = (lineInfo): any => {
+    if (!lineInfo) {
+      return;
     }
 
-    public dispose(): void {
-        this.extension.dispose();
+    const isNotAnumber =
+      isNaN(parseInt(lineInfo.position, 10)) ||
+      isNaN(parseInt(lineInfo.column, 10));
+    const isLessThanOrEqualToZero =
+      lineInfo.position <= 0 || lineInfo.column <= 0;
+
+    if (isNotAnumber || isLessThanOrEqualToZero) {
+      return;
     }
 
-    // getDiagnosis for vscode.diagnostics
-    public getDiagnosis(item, vscode= this.vscode): any {
-        const range = new vscode.Range(
-            item.startLine,
-            item.startColumn,
-            item.endLine,
-            item.endColumn,
-        );
-        const itemSeverity = severity.parse(item.check, vscode);
-        const message = `${item.message} [${item.check}:${itemSeverity}]`;
-        return new vscode.Diagnostic(range, message, itemSeverity);
+    return {
+      check: lineInfo.check,
+      endColumn: this.makeZeroIndex(lineInfo.column),
+      endLine: this.makeZeroIndex(lineInfo.position),
+      message: lineInfo.message,
+      startColumn: 0,
+      startLine: this.makeZeroIndex(lineInfo.position),
+    };
+  }
+
+  public parseOutput(output) {
+    return parse.getLines(output).map((error) => {
+      const lineInfo = parse.getLineInfo(error);
+
+      return this.getDiagnosticInfo(lineInfo);
+    });
+  }
+
+  public parseInfoOutput(output): string[] {
+    const json = parse.getLines(output).join("\n");
+    const info = JSON.parse(json);
+    return info.config ? info.config.files : [];
+  }
+
+  /**
+   * Using cp.spawn(), extensions can call any executable and process the results.
+   * The code below uses cp.spawn() to call linter, parses the output into Diagnostic objects
+   * and then adds them to a DiagnosticCollection with this.diagnosticCollection.set(textDocument.uri, diagnostics);
+   * which add the chrome in the UI.
+   */
+
+  private linter(textDocument: any, index, vscode = this.vscode) {
+    if (textDocument.languageId !== "elixir") {
+      return;
     }
 
-    public makeZeroIndex = (value: number): number => {
-        if (value <= 0) {
-            return 0;
-        }
+    this.getLintedFiles(vscode, (files) => {
+      if (files.indexOf(textDocument.fileName) === -1) {
+        return;
+      }
 
-        return value - 1;
+      let args = ["credo", "list", "--format=oneline", "--read-from-stdin"];
+
+      const settings = vscode.workspace.getConfiguration("elixirLinter");
+      if (settings.useStrict === true) {
+        args = args.concat("--strict");
+      }
+
+      // use stdin for credo to prevent running on entire project
+      const childProcess = execa(
+        ElixirLintingProvider.linterCommand,
+        args,
+        cmd.getOptions(vscode),
+      );
+      childProcess.stdin.write(textDocument.getText());
+      childProcess.stdin.end();
+
+      childProcess.then((decoded) => {
+        this.handleFileDiagnosticOutput(decoded, textDocument, vscode);
+      });
+
+      //   childProcess.stdout.on("data", (data: Buffer) => {
+      //         decoded += data;
+      //     });
+      //   childProcess.stdout.on("end", () => this.handleFileDiagnosticOutput(decoded, textDocument, vscode))
+
+      //   stream.pipe(textDocument.getText());
+      //   getStream(stream)
+      //     .then(stdout => this.handleFileDiagnosticOutput(stdout, textDocument, vscode))
+      //     .catch(error => this.handleFileDiagnosticError(error));
+
+      //   if (childProcess.pid) {
+      //       childProcess.stdout.on("data", (data: Buffer) => {
+      //           decoded += data;
+      //       });
+      //       childProcess.stdout.on("end", () => this.handleFileDiagnosisOutput(decoded, textDocument, vscode));
+      //   }
+    });
+  }
+
+  private handleFileDiagnosticError(error) {
+    console.warn(`Elixir linter error: ${error}`);
+  }
+
+  private handleFileDiagnosticOutput(decoded, textDocument, vscode) {
+    const diagnostics: any[] = [];
+
+    this.parseOutput(decoded).forEach((item) => {
+      if (item) {
+        diagnostics.push(this.getDiagnosis(item, vscode));
+      }
+    });
+    this.diagnosticCollection.set(textDocument.uri, diagnostics);
+  }
+
+  private handleCredoFileOutput(decoded, callback, vscode) {
+    const files = this.parseInfoOutput(decoded).map((item) => {
+      return path.join(vscode.workspace.rootPath, item);
+    });
+    callback(files);
+  }
+
+  private getLintedFiles(
+    vscode = this.vscode,
+    callback: (files: string[]) => void,
+  ) {
+    let decoded = "";
+    const args = ["credo", "info", "--verbose", "--format=json"];
+
+    const childProcess = execa
+      .stdout(ElixirLintingProvider.linterCommand, args, cmd.getOptions(vscode))
+      .then((stdout) => this.handleCredoFileOutput(stdout, callback, vscode));
+
+    if (childProcess.pid) {
+      childProcess.stdout.on("data", (data: Buffer) => {
+        decoded += data;
+      });
+      childProcess.stdout.on("end", () =>
+        this.handleCredoFileOutput(decoded, callback, vscode),
+      );
+    } else {
+      callback([]);
     }
-
-    public getDiagnosticInfo = (lineInfo): any => {
-        if (!lineInfo) {
-            return;
-        }
-
-        const isNotAnumber = isNaN(parseInt(lineInfo.position, 10)) || isNaN(parseInt(lineInfo.column, 10));
-        const isLessThanOrEqualToZero = lineInfo.position <= 0 || lineInfo.column <= 0;
-
-        if (isNotAnumber || isLessThanOrEqualToZero) {
-            return;
-        }
-
-        return {
-            check: lineInfo.check,
-            endColumn: this.makeZeroIndex(lineInfo.column),
-            endLine: this.makeZeroIndex(lineInfo.position),
-            message: lineInfo.message,
-            startColumn: 0,
-            startLine: this.makeZeroIndex(lineInfo.position),
-        };
-    }
-
-    public parseOutput(output) {
-        return parse.getLines(output).map((error) => {
-            const lineInfo = parse.getLineInfo(error);
-
-            return this.getDiagnosticInfo(lineInfo);
-        });
-    }
-
-    public parseInfoOutput(output): string[] {
-        const json = parse.getLines(output).join("\n");
-        const info = JSON.parse(json);
-        return info.config ? info.config.files : [];
-    }
-
-    /**
-     * Using cp.spawn(), extensions can call any executable and process the results.
-     * The code below uses cp.spawn() to call linter, parses the output into Diagnostic objects
-     * and then adds them to a DiagnosticCollection with this.diagnosticCollection.set(textDocument.uri, diagnostics);
-     * which add the chrome in the UI.
-     */
-
-    private linter(textDocument: any, index, vscode = this.vscode) {
-        if (textDocument.languageId !== "elixir") {
-            return;
-        }
-
-        this.getLintedFiles(vscode, (files) => {
-          if (files.indexOf(textDocument.fileName) === -1) {
-            return;
-          }
-
-          let decoded = "";
-          let args =  ["credo", "list", "--format=oneline", "--read-from-stdin"];
-
-          const settings = vscode.workspace.getConfiguration("elixirLinter");
-          if (settings.useStrict === true) {
-              args = args.concat("--strict");
-          }
-
-          // use stdin for credo to prevent running on entire project
-          const stream = execa(ElixirLintingProvider.linterCommand, args, cmd.getOptions(vscode)).stdout;
-
-          stream.pipe(textDocument.getText());
-          getStream(stream)
-            .then(stdout => this.handleFileDiagnosisOutput(stdout, textDocument, vscode))
-            .catch(error => console.warn(`Elixir linter error: ${error}`));
-
-        //   if (childProcess.pid) {
-        //       childProcess.stdout.on("data", (data: Buffer) => {
-        //           decoded += data;
-        //       });
-        //       childProcess.stdout.on("end", () => this.handleFileDiagnosisOutput(decoded, textDocument, vscode));
-        //   }
-        });
-    }
-
-    private handleFileDiagnosisOutput(decoded, textDocument, vscode) {
-        const diagnostics: any[] = [];
-
-        this.parseOutput(decoded).forEach( (item) => {
-            if (item) {
-                diagnostics.push(this.getDiagnosis(item, vscode));
-            }
-        });
-        this.diagnosticCollection.set(textDocument.uri, diagnostics);
-    }
-
-    private handleCredoFileOutput(decoded, callback, vscode) {
-        const files = this.parseInfoOutput(decoded).map((item) => {
-            return path.join(vscode.workspace.rootPath, item);
-        });
-        callback(files);
-    }
-
-    private getLintedFiles(vscode = this.vscode, callback: (files: string[]) => void) {
-        let decoded = "";
-        const args =  ["credo", "info", "--verbose", "--format=json"];
-
-        const childProcess = execa.stdout(ElixirLintingProvider.linterCommand, args, cmd.getOptions(vscode)).then((stdout) => this.handleCredoFileOutput(stdout, callback, vscode));
-
-        if (childProcess.pid) {
-            childProcess.stdout.on("data", (data: Buffer) => {
-                decoded += data;
-            });
-            childProcess.stdout.on("end", () => this.handleCredoFileOutput(decoded, callback, vscode));
-        } else {
-          callback([]);
-        }
-    }
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,142 @@
-"use strict";
-
-/**
- * When the extension is activated by VS Code it activates its own helper to handle
- * the linting and then registers that helper as a code action provider.
- */
-
+import * as crossSpawn from "cross-spawn";
+import * as execa from "execa";
+import * as findUp from "find-up";
+import * as path from "path";
+import * as shelljs from "shelljs";
 import * as vscode from "vscode";
 
-import ElixirLintingProvider from "./credoProvider";
+import { output } from "../test/fixtures";
+import * as severity from "./severity";
+
+const childProcess = require("child_process");
 
 export function activate(context: vscode.ExtensionContext) {
-    const linter = new ElixirLintingProvider(vscode);
-    linter.activate(context.subscriptions, vscode);
+  const collection = vscode.languages.createDiagnosticCollection("test");
+  if (vscode.window.activeTextEditor) {
+    updateDiagnostics(vscode.window.activeTextEditor.document, collection);
+  }
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor((e) =>
+      updateDiagnostics(e.document, collection),
+    ),
+  );
+}
+
+type CredoIssue = {
+  trigger: null;
+  priority: number;
+  message: string;
+  line_no: null;
+  filename: string;
+  column_end: null;
+  check: string;
+  category: string;
+};
+
+type ShowDiagnostics = {
+  collection: vscode.DiagnosticCollection;
+  directory: string;
+  issues: CredoIssue[];
+  cwd: string;
+};
+
+type Entry = [vscode.Uri, [vscode.Diagnostic]];
+
+// {
+//   "trigger": null,
+//   "priority": 10,
+//   "message": "File is using unix line endings while most of the files use windows line endings.",
+//   "line_no": null,
+//   "filename": "web/web.ex",
+//   "column_end": null,
+//   "column": null,
+//   "check": "Credo.Check.Consistency.LineEndings",
+//   "category": "consistency"
+// }
+function showDiagnostics({ collection, directory, issues, cwd }: ShowDiagnostics) {
+  const severityCode = {
+    consistency: "C",
+    design: "D",
+    readability: "R",
+    refactoring: "F",
+    warnings: "W",
+  };
+
+  const entries = issues.map((i: CredoIssue): Entry => {
+    // const s = severity.parse(severityCode[output.category], vscode);
+
+    const filename = i.filename.replace(/\\/gi, "\/");
+    const rootPath = cwd.replace(/\\/gi, "\/");
+    const fullPath = `/${rootPath}\/${filename}`;
+    const uri = vscode.Uri.parse(fullPath);
+    // uri.scheme = "file";
+    const defaultLine = 1;
+    const defaultColumn = 1;
+    const range = new vscode.Range(
+      new vscode.Position(1, 1),
+      new vscode.Position(1, 1),
+    );
+    const info = new vscode.Diagnostic(range, i.message, vscode.DiagnosticSeverity.Error);
+    const targetFile = path.basename(i.filename);
+    return [uri, [info]];
+  });
+
+  collection.set(entries);
+}
+
+function logError(error) {
+  vscode.debug.activeDebugConsole.appendLine(error.stderr || error);
+}
+
+async function updateDiagnostics(
+  document: vscode.TextDocument,
+  collection: vscode.DiagnosticCollection,
+): Promise<any> {
+  if (!document || document.languageId !== "elixir") {
+    return collection.clear();
+  }
+
+  const currentUri = document.uri;
+  const documentPath = path.basename(document.uri.fsPath);
+  const directory = await findUp("mix.exs", { cwd: document.uri.fsPath });
+
+  runDiagnosticts({ directory, collection });
+}
+
+function getIssues({ cwd, directory}): Promise<CredoIssue[]> {
+  const args = ["credo", "list", "--format=json"];
+
+  vscode.debug.activeDebugConsole.appendLine(`Running mix ${args} in ${cwd}`);
+  return new Promise((resolve, reject) => {
+    childProcess.exec(`mix ${args.join(" ")}`, { cwd, env: {MIX_EXS: directory}}, (error, stdout, stderror) => {
+      if (!stdout) {
+        reject(error || "No credo issues found");
+      }
+      const pwd = shelljs.pwd();
+      resolve(JSON.parse(stdout).issues);
+    });
+  });
+}
+
+type RunDiagnostics = {
+  directory: string,
+  collection: vscode.DiagnosticCollection,
+};
+
+async function runDiagnosticts({directory, collection}: RunDiagnostics): Promise<any> {
+  if (!directory) {
+    vscode.debug.activeDebugConsole.appendLine("No mix.exs found in current directory or parents");
+  }
+
+  const cwd = path.dirname(directory);
+  try {
+    const issues = await getIssues({ cwd, directory });
+    showDiagnostics({ issues, collection, directory, cwd });
+  } catch (error) {
+    collection.clear();
+    return logError(error);
+  }
+
+  // execa("mix", args, { cwd, env: {MIX_EXS: mixDirectory}})
+  //   .finally((output) => showDiagnostics({ output, collection, uri: document.uri }));
 }
